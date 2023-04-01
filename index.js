78194a68bcb96c32a382d0cf901f63045945fc8d
Shizuku22 <122438503+Shizuku22@users.noreply.github.com>
2023-04-01 13:52:51 +0800

    edit links

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-  // Get the H1 element by its ID
-  const nameElement = document.getElementById("name");
+  // // Get the H1 element by its ID
+  // const nameElement = document.getElementById("name");
 
-  // Define the text to be typed
-  const nameText = "Cha Arellano";
+  // // Define the text to be typed
+  // const nameText = "Cha Arellano";
 
-  // Set the initial text of the H1 element
-  nameElement.textContent = "";
+  // // Set the initial text of the H1 element
+  // nameElement.textContent = "";
 
-  // Start the typing animation
-  let i = 0;
-  function type() {
-    if (i < nameText.length) {
-      nameElement.textContent += nameText.charAt(i);
-      i++;
-      setTimeout(type, 100); // Change the typing speed by adjusting this value
-    }
-  }
-  type();
+  // // Start the typing animation
+  // let i = 0;
+  // function type() {
+  //   if (i < nameText.length) {
+  //     nameElement.textContent += nameText.charAt(i);
+  //     i++;
+  //     setTimeout(type, 100); // Change the typing speed by adjusting this value
+  //   }
+  // }
+  // type();
 
 
